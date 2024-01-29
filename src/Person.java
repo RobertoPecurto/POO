919241d9ebedb904ae88e3a012a1ec49df5da062
RobertoPecurto <robpecesc@alu.edu.gva.es>
2024-01-29 09:07:22 +0100

    POO

---
+++ src/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    private String nombre;
+    private String direccion;
+
+    public Person (String nombre){
+        this.nombre = nombre;
+    }
+
+    Person (String nombre, String direccion){
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString(){
+        return this.nombre + "\n\t" + this.direccion;
+    }
+}
